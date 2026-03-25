c18e723f0385c89fa3d9ca759b98af345569351d
ScriptSquirrel216 <ali.hamas@teams.pk>
2026-03-26 00:09:11 +0500

    chore: update dependencies and devDependencies for improved performance and compatibility, including major version upgrades for several packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
-// import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import oxlintPlugin from "vite-plugin-oxlint";
-import viteTsConfigPaths from "vite-tsconfig-paths";
-
-const config = defineConfig({
-	plugins: [
-		// devtools(),
-		nitro(),
-		viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
-		tailwindcss(),
-		tanstackStart(),
-		viteReact(),
-		oxlintPlugin({ path: "src" }),
-	],
-});
 
 // oxlint-disable no-default-export
-export default config;
+export default defineConfig({
+	resolve: { tsconfigPaths: true },
+	plugins: [nitro(), tailwindcss(), tanstackStart(), viteReact(), oxlintPlugin({ path: "src" })]
+});
